b8397028f799220856f8f5c51d083d478864b0ed
yuvrajkrishna <yuvrajkrishna26@gmail.com>
2026-07-12 17:13:57 +0530

    thirteen

---
+++ thirteen.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main ( ){
+
+    int n = 5 ;
+    int val = 1;
+
+    for(int i = 1 ; i <= n ; i++){
+        
+        for(int j = 1 ; j <= i; j++){
+            printf("%d ",val);
+            val++;
+        }
+        printf("\n");
+    }
+
+    return 0;
+
+}

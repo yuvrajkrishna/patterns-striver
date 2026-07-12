010cf45427417509c4434531c95d2d99b5a15f54
yuvrajkrishna <yuvrajkrishna26@gmail.com>
2026-07-12 17:22:55 +0530

    fifteen

---
+++ fifteen.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main ( ) {
+
+    int n = 5 ;
+
+    for(int i = 1 ; i <= n ; i++) {
+        char ch = 'A';
+        for(int j = 1 ; j <=n-i+1; j++){
+            printf("%c ",ch);
+            ch++;
+        }
+        printf("\n");
+    }
+
+    return 0 ; 
+
+}
